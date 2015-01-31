48f71bc55d19488d0f67f3221446c029a271a61f
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-01-31 23:17:39 +0200

    omit needless semicolon after class definitions

--- templates/controller.js
+++ templates/controller.js
@@ -2,6 +2,6 @@ class %CONTROLLER_NAME% {
   constructor () {
     this.controllerName = '%CONTROLLER_NAME%'
   }
-};
+}
 
 angular.module('%APP_NAME%.controllers').controller('%CONTROLLER_NAME%', %CONTROLLER_NAME%);
--- templates/state/state_controller.js
+++ templates/state/state_controller.js
@@ -3,6 +3,6 @@ class %STATE_CONTROLLER_NAME%Controller {
     this.stateUrl = '/%STATE_NAME%';
     this.controllerName = '%STATE_CONTROLLER_NAME%Controller'
   }
-};
+}
 
 angular.module('%APP_NAME%.controllers').controller('%STATE_CONTROLLER_NAME%Controller', %STATE_CONTROLLER_NAME%Controller);
