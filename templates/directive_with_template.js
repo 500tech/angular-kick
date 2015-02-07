bd26075c9ba0432abb8df2dd33d7e8306a7df17e
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-02-07 09:54:48 +0200

    add option to generate directive with template

---
+++ templates/directive_with_template.js
@@ -0,0 +1,16 @@
+export class %DIRECTIVE_NAME%Controller {
+  constructor($element) {
+    console.log($element);
+  }
+}
+
+angular.module('%APP_NAME%.directives').directive('%DIRECTIVE_NAME%', function () {
+  return {
+    restrict: 'EA',
+    scope: {},
+    templateUrl: '%DIRECTIVE_TEMPLATE_URL%',
+    controller: %DIRECTIVE_NAME%Controller,
+    controllerAs: '%DIRECTIVE_NAME%',
+    bindToController: true
+  }
+});
