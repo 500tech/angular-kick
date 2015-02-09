13d8873849ee4be1777480935c216151a98c8b23
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-02-09 09:02:11 +0200

    revert back module dependency

--- templates/app/app/modules.js
+++ templates/app/app/modules.js
@@ -1,4 +1,6 @@
-angular.module('%APP_NAME%.config', []);
+angular.module('%APP_NAME%.config', [
+  'ui.router'
+]);
 angular.module('%APP_NAME%.services', []);
 angular.module('%APP_NAME%.filters', []);
 angular.module('%APP_NAME%.directives', []);
