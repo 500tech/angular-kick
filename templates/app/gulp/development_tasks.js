42892ee1477ee44d9b671aeb4be402418521c799
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-02-11 21:29:01 +0200

    refactoring long functions

--- templates/app/gulp/development_tasks.js
+++ templates/app/gulp/development_tasks.js
@@ -9,16 +9,39 @@ var modRewrite  = require('connect-modrewrite');
 var browserSync = require('browser-sync');
 var karma       = require('karma').server;
 var streams     = require('stream-series');
-
 var common      = require('./common');
 var sources     = common.sources;
 var setENV      = common.setENV;
 var destination = common.destinations.development;
-var ENV = ENV || setENV['development'];
-
+var ENV         = ENV || setENV['development'];
 var errorRaised = false;
 
-var catchError = function (err) {
+module.exports = {
+  clean:          clean,
+  setEnvironment: setEnvironment,
+  fonts:          fonts,
+  styles:         styles,
+  index:          index,
+  views:          views,
+  images:         images,
+  vendor:         vendor,
+  vendorConcat:   vendorConcat,
+  scripts:        scripts,
+  test:           test,
+  testEnv:        testEnv,
+  server:         server,
+  serverTdd:      serverTdd,
+  serverEnv:      serverEnv,
+  serverEnvTdd:   serverEnvTdd,
+  tdd:            tdd,
+  testOnce:       testOnce,
+  reloadScripts:  reloadScripts,
+  reloadViews:    reloadViews,
+  reloadStyles:   reloadStyles,
+  serve:          serve
+};
+
+function catchError(err) {
   plugins.util.beep();
   console.log(err);
   errorRaised = true;
@@ -42,177 +65,183 @@ var catchError = function (err) {
       }
     }))
     .pipe(gulp.dest(destination));
-};
+}
 
-module.exports = {
-  clean: function () {
-    return gulp.src(destination, { read: false })
-      .pipe(plugins.rimraf());
-  },
+function clean() {
+  return gulp.src(destination, {read: false})
+    .pipe(plugins.rimraf());
+}
 
-  setEnvironment: function () {
-    var environment = process.argv[3].replace(/^--/, '');
-    if (setENV[environment]) {
-      console.log('Setting ENV to ' + environment);
-      ENV = setENV[environment];
-    } else {
-      throw 'Environment "' + environment + '" was not found in environments.js file';
-    }
-  },
-
-  fonts: function () {
-    return gulp.src(sources.fonts, { base: sources.base })
-      .pipe(plugins.changed(destination, {
-        hasChanged: plugins.changed.compareSha1Digest
-      }))
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(gulp.dest(destination));
-  },
-
-  styles: function () {
-    return gulp.src(sources.mainStyle, { base: sources.base })
-      .pipe(plugins.changed(destination, {
-        hasChanged: plugins.changed.compareSha1Digest
-      }))
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.sass({
-        onError: catchError
-      }))
-      .pipe(plugins.autoprefixer({ browsers: ['last 2 versions'] }))
-      .pipe(plugins.sourcemaps.write())
-      .pipe(gulp.dest(destination));
-  },
-
-  index: function () {
-    if (errorRaised) {
-      errorRaised = false;
-      return;
-    }
-
-    var app         = gulp.src(sources.app, { read: false });
-    var modules     = gulp.src(sources.modules, { read: false });
-    var configs     = gulp.src(sources.configs, { read: false });
-    var directives  = gulp.src(sources.directives, { read: false });
-    var filters     = gulp.src(sources.filters, { read: false });
-    var services    = gulp.src(sources.services, { read: false });
-    var controllers = gulp.src(sources.controllers, { read: false });
-    var vendorFiles = gulp.src(sources.vendorFiles, { read: false });
-
-    return gulp.src(sources.index)
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.inject(streams(vendorFiles, modules, app, configs, directives, filters, services, controllers), { relative: true }))
-      .pipe(plugins.replace(/\.\.\//g, ''))
-      .pipe(plugins.replace('app/', ''))
-      .pipe(gulp.dest(destination));
-  },
-
-  views: function () {
-    return gulp.src(sources.views, { base: sources.base })
-      .pipe(plugins.changed(destination, {
-        hasChanged: plugins.changed.compareSha1Digest
-      }))
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(gulp.dest(destination))
-  },
-
-  images: function () {
-    return gulp.src(sources.images, { base: sources.base })
-      .pipe(plugins.changed(destination, {
-        hasChanged: plugins.changed.compareSha1Digest
-      }))
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(gulp.dest(destination));
-  },
-
-  vendor: function () {
-    return gulp.src(sources.vendorFiles, { base: sources.base })
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(gulp.dest(destination + '/vendor'));
-  },
-
-  vendorConcat: function () {
-    return gulp.src(sources.vendor)
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.include({ extensions: ['js'] }))
-      .pipe(gulp.dest(destination));
-  },
-
-  scripts: function () {
-    return gulp.src(sources.scripts)
-      .pipe(plugins.changed(destination, {
-        hasChanged: plugins.changed.compareSha1Digest
-      }))
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.replaceTask({ patterns: [{ json: ENV }] }))
-      .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.traceur({ modules: 'register' }))
-      .pipe(plugins.ngAnnotate())
-      .pipe(plugins.sourcemaps.write('.'))
-      .pipe(gulp.dest(destination));
-  },
-
-  test: function () {
-    destination = common.destinations.test;
-    return run('clean', 'vendorConcat', 'scripts', 'styles', 'images', 'views', 'index', 'fonts', 'testOnce', 'clean');
-  },
-
-  testEnv: function () {
-    return run('setEnvironment', 'test');
-  },
-
-  server: function () {
-    return run('clean', 'vendorConcat', 'vendor', 'scripts', 'styles', 'images', 'views', 'index', 'fonts', 'serve');
-  },
-
-  serverTdd: function () {
-    return run('server', 'tdd');
-  },
-
-  serverEnv: function () {
-    return run('setEnvironment', 'server');
-  },
-
-  serverEnvTdd: function () {
-    return run('setEnvironment', 'server:tdd');
-  },
-
-  tdd: function (done) {
-    return karma.start({
-      configFile: __dirname + '/../karma.conf.js',
-      singleRun: false
-    }, done);
-  },
-
-  testOnce: function (done) {
-    return karma.start({
-      configFile: __dirname + '/../karma-once.conf.js',
-      singleRun: true
-    }, done);
-  },
-
-  reloadScripts: function () { return run('scripts', 'views', 'index');},
-  reloadViews : function ()  { return run('views', 'index');},
-  reloadStyles: function ()  { return run('styles', 'index');},
-
-  serve: function () {
-    var proxyOptions;
-    proxyOptions = url.parse(ENV['API_URL']);
-    proxyOptions.route = '/api';
-    browserSync({
-      browser: ['google chrome', 'chrome'],
-      notify: false,
-      port: 3001,
-      server: {
-        baseDir: '.tmp',
-        middleware: [proxy(proxyOptions), modRewrite(['!\\.\\w+$ /app.html [L]'])]
-      }
-    });
-    gulp.watch(sources.scripts, ['reloadScripts', browserSync.reload]);
-    gulp.watch(sources.views,   ['reloadViews',   browserSync.reload]);
-    gulp.watch(sources.index,   ['reloadViews',   browserSync.reload]);
-    gulp.watch(sources.styles,  ['reloadStyles',  browserSync.reload]);
-    gulp.watch(sources.images,  ['images',        browserSync.reload]);
-    gulp.watch(sources.fonts,   ['fonts',         browserSync.reload]);
+function setEnvironment() {
+  var environment = process.argv[3].replace(/^--/, '');
+  if (setENV[environment]) {
+    console.log('Setting ENV to ' + environment);
+    ENV = setENV[environment];
+  } else {
+    throw 'Environment "' + environment + '" was not found in environments.js file';
   }
-};
+}
+
+function fonts() {
+  return gulp.src(sources.fonts, {base: sources.base})
+    .pipe(plugins.changed(destination, {
+      hasChanged: plugins.changed.compareSha1Digest
+    }))
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(gulp.dest(destination));
+}
+
+function styles() {
+  return gulp.src(sources.mainStyle, {base: sources.base})
+    .pipe(plugins.changed(destination, {
+      hasChanged: plugins.changed.compareSha1Digest
+    }))
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass({
+      onError: catchError
+    }))
+    .pipe(plugins.autoprefixer({browsers: ['last 2 versions']}))
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest(destination));
+}
+
+function index() {
+  if (errorRaised) {
+    errorRaised = false;
+    return;
+  }
+
+  var app = gulp.src(sources.app, {read: false});
+  var modules = gulp.src(sources.modules, {read: false});
+  var configs = gulp.src(sources.configs, {read: false});
+  var directives = gulp.src(sources.directives, {read: false});
+  var filters = gulp.src(sources.filters, {read: false});
+  var services = gulp.src(sources.services, {read: false});
+  var controllers = gulp.src(sources.controllers, {read: false});
+  var vendorFiles = gulp.src(sources.vendorFiles, {read: false});
+
+  return gulp.src(sources.index)
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(plugins.inject(streams(vendorFiles, modules, app, configs, directives, filters, services, controllers), {relative: true}))
+    .pipe(plugins.replace(/\.\.\//g, ''))
+    .pipe(plugins.replace('app/', ''))
+    .pipe(gulp.dest(destination));
+}
+
+function views() {
+  return gulp.src(sources.views, {base: sources.base})
+    .pipe(plugins.changed(destination, {
+      hasChanged: plugins.changed.compareSha1Digest
+    }))
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(gulp.dest(destination))
+}
+
+function images() {
+  return gulp.src(sources.images, {base: sources.base})
+    .pipe(plugins.changed(destination, {
+      hasChanged: plugins.changed.compareSha1Digest
+    }))
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(gulp.dest(destination));
+}
+
+function vendor() {
+  return gulp.src(sources.vendorFiles, {base: sources.base})
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(gulp.dest(destination + '/vendor'));
+}
+
+function vendorConcat() {
+  return gulp.src(sources.vendor)
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(plugins.include({extensions: ['js']}))
+    .pipe(gulp.dest(destination));
+}
+
+function scripts() {
+  return gulp.src(sources.scripts)
+    .pipe(plugins.changed(destination, {
+      hasChanged: plugins.changed.compareSha1Digest
+    }))
+    .pipe(plugins.plumber({errorHandler: catchError}))
+    .pipe(plugins.replaceTask({patterns: [{json: ENV}]}))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.traceur({modules: 'register'}))
+    .pipe(plugins.ngAnnotate())
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest(destination));
+}
+
+function test() {
+  destination = common.destinations.test;
+  return run('clean', 'vendorConcat', 'scripts', 'styles', 'images', 'views', 'index', 'fonts', 'testOnce', 'clean');
+}
+
+function testEnv() {
+  return run('setEnvironment', 'test');
+}
+
+function server() {
+  return run('clean', 'vendorConcat', 'vendor', 'scripts', 'styles', 'images', 'views', 'index', 'fonts', 'serve');
+}
+
+function serverTdd() {
+  return run('server', 'tdd');
+}
+
+function serverEnv() {
+  return run('setEnvironment', 'server');
+}
+
+function serverEnvTdd() {
+  return run('setEnvironment', 'server:tdd');
+}
+
+function tdd(done) {
+  return karma.start({
+    configFile: __dirname + '/../karma.conf.js',
+    singleRun: false
+  }, done);
+}
+
+function testOnce(done) {
+  return karma.start({
+    configFile: __dirname + '/../karma-once.conf.js',
+    singleRun: true
+  }, done);
+}
+
+function reloadScripts() {
+  return run('scripts', 'views', 'index');
+}
+
+function reloadViews() {
+  return run('views', 'index');
+}
+
+function reloadStyles() {
+  return run('styles', 'index');
+}
+
+function serve() {
+  var proxyOptions;
+  proxyOptions = url.parse(ENV['API_URL']);
+  proxyOptions.route = '/api';
+  browserSync({
+    browser: ['google chrome', 'chrome'],
+    notify: false,
+    port: 3001,
+    server: {
+      baseDir: '.tmp',
+      middleware: [proxy(proxyOptions), modRewrite(['!\\.\\w+$ /app.html [L]'])]
+    }
+  });
+  gulp.watch(sources.scripts, ['reloadScripts', browserSync.reload]);
+  gulp.watch(sources.views, ['reloadViews', browserSync.reload]);
+  gulp.watch(sources.index, ['reloadViews', browserSync.reload]);
+  gulp.watch(sources.styles, ['reloadStyles', browserSync.reload]);
+  gulp.watch(sources.images, ['images', browserSync.reload]);
+  gulp.watch(sources.fonts, ['fonts', browserSync.reload]);
+}
--- templates/app/gulp/production_tasks.js
+++ templates/app/gulp/production_tasks.js
@@ -1,148 +1,158 @@
 "use strict";
 
-var gulp    = require('gulp');
-var plugins = require('gulp-load-plugins')();
-var fs      = require('fs-extra');
-var run     = require('run-sequence');
-
+var gulp        = require('gulp');
+var plugins     = require('gulp-load-plugins')();
+var fs          = require('fs-extra');
+var run         = require('run-sequence');
 var common      = require('./common');
 var sources     = common.sources;
 var setENV      = common.setENV;
 var destination = common.destinations.production;
-
-var ENV = setENV['production'];
-
-var catchError = function (err) {
-  plugins.util.beep();
-  return console.log(err);
-};
+var ENV         = setENV['production'];
 
 module.exports = {
-  clean: function () {
-    return gulp.src(destination, { read: false })
-      .pipe(plugins.rimraf());
-  },
-
-  setEnvironment: function () {
-    var environment = process.argv[3].replace(/^--/, '');
-    if (setENV[environment]) {
-      console.log('Setting ENV to ' + environment);
-      ENV = setENV[environment];
-    } else {
-      throw 'Environment "' + environment + '" was not found in environments.js file';
-    }
-  },
-
-  fonts: function () {
-    return gulp.src(sources.fonts, { base: sources.base })
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(gulp.dest(destination));
-  },
-
-  styles: function () {
-    return gulp.src(sources.mainStyle, { base: sources.base })
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.replaceTask({ patterns: [{ json: ENV }] }))
-      .pipe(plugins.sass())
-      .pipe(plugins.autoprefixer({ browsers: ['last 2 versions'] }))
-      .pipe(plugins.cssmin())
-      .pipe(plugins.rev())
-      .pipe(gulp.dest(destination))
-      .pipe(plugins.rev.manifest({ path: 'manifests/styles.json' }))
-      .pipe(gulp.dest(destination));
-  },
-
-  index: function () {
-    var manifest          = JSON.parse(fs.readFileSync(destination + '/manifests/scripts.json', "utf8"));
-    var configManifest    = JSON.parse(fs.readFileSync(destination + '/manifests/config.json', "utf8"));
-    var templateManifest  = JSON.parse(fs.readFileSync(destination + '/manifests/templates.json', "utf8"));
-    var styleManifest   = JSON.parse(fs.readFileSync(destination + '/manifests/styles.json', "utf8"));
-    var applicationSCSS = styleManifest[Object.keys(styleManifest)[0]].match(/application-.*\.css/)[0];
-    return gulp.src(sources.index)
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.replace('app.js', manifest['app.js']))
-      .pipe(plugins.replace('vendor.js', configManifest['vendor.js']))
-      .pipe(plugins.replace('templates.js', templateManifest['templates.js']))
-      .pipe(plugins.replace('application.css', applicationSCSS))
-      .pipe(plugins.minifyHtml({
-        empty: true,
-        comments: true,
-        quotes: true,
-        spare: true,
-        conditionals: true
-      }))
-      .pipe(gulp.dest(destination));
-  },
-
-  views: function () {
-    var appName = JSON.parse(fs.readFileSync(__dirname + '/../package.json', "utf8"))['name'];
-    return gulp.src(sources.views, { base: sources.base })
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.replaceTask({ patterns: [{ json: ENV }] }))
-      .pipe(plugins.minifyHtml({
-        empty: true,
-        comments: true,
-        quotes: true,
-        spare: true,
-        conditionals: true
-      }))
-      .pipe(plugins.html2js({
-        outputModuleName: appName + '.templates',
-        base: 'app/'
-      }))
-      .pipe(plugins.concat('templates.js'))
-      .pipe(plugins.ngAnnotate())
-      .pipe(plugins.uglify())
-      .pipe(plugins.rev())
-      .pipe(gulp.dest(destination))
-      .pipe(plugins.rev.manifest({ path: 'manifests/templates.json' }))
-      .pipe(gulp.dest(destination));
-  },
-
-  images: function () {
-    return gulp.src(sources.images, { base: sources.base })
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.imagemin({ progressive: true }))
-      .pipe(gulp.dest(destination));
-  },
-
-  vendor: function () {
-    return gulp.src(sources.vendor)
-      .pipe(plugins.include({ extensions: ['js'] }))
-      .pipe(plugins.rev())
-      .pipe(gulp.dest(destination))
-      .pipe(plugins.rev.manifest({ path: 'manifests/config.json' }))
-      .pipe(gulp.dest(destination));
-  },
-
-  scripts: function () {
-    return gulp.src(sources.app)
-      .pipe(plugins.plumber({ errorHandler: catchError }))
-      .pipe(plugins.include({ extensions: ['js'] }))
-      .pipe(plugins.traceur({ modules: 'register' }))
-      .pipe(plugins.ngAnnotate())
-      .pipe(plugins.uglify())
-      .pipe(plugins.replaceTask({ patterns: [{ json: ENV }] }))
-      .pipe(plugins.rev())
-      .pipe(gulp.dest(destination))
-      .pipe(plugins.rev.manifest({ path: 'manifests/scripts.json' }))
-      .pipe(gulp.dest(destination));
-  },
-
-  build: function () {
-    return run(
-      'productionClean',
-      'productionVendor',
-      'productionScripts',
-      'productionStyles',
-      'productionImages',
-      'productionViews',
-      'productionIndex',
-      'productionFonts'
-    );
-  },
-
-  buildEnv: function () {
-    return run('productionSetEnvironment', 'build');
-  }
+  clean:          clean,
+  setEnvironment: setEnvironment,
+  fonts:          fonts,
+  styles:         styles,
+  index:          index,
+  views:          views,
+  images:         images,
+  vendor:         vendor,
+  scripts:        scripts,
+  build:          build,
+  buildEnv:       buildEnv
 };
+
+function catchError(err) {
+  plugins.util.beep();
+  return console.log(err);
+}
+
+function clean() {
+  return gulp.src(destination, { read: false })
+    .pipe(plugins.rimraf());
+}
+
+function setEnvironment() {
+  var environment = process.argv[3].replace(/^--/, '');
+  if (setENV[environment]) {
+    console.log('Setting ENV to ' + environment);
+    ENV = setENV[environment];
+  } else {
+    throw 'Environment "' + environment + '" was not found in environments.js file';
+  }
+}
+
+function fonts() {
+  return gulp.src(sources.fonts, { base: sources.base })
+    .pipe(plugins.plumber({ errorHandler: catchError }))
+    .pipe(gulp.dest(destination));
+}
+
+function styles() {
+  return gulp.src(sources.mainStyle, { base: sources.base })
+    .pipe(plugins.plumber({ errorHandler: catchError }))
+    .pipe(plugins.replaceTask({ patterns: [{ json: ENV }] }))
+    .pipe(plugins.sass())
+    .pipe(plugins.autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(plugins.cssmin())
+    .pipe(plugins.rev())
+    .pipe(gulp.dest(destination))
+    .pipe(plugins.rev.manifest({ path: 'manifests/styles.json' }))
+    .pipe(gulp.dest(destination));
+}
+
+function index() {
+  var manifest          = JSON.parse(fs.readFileSync(destination + '/manifests/scripts.json', "utf8"));
+  var configManifest    = JSON.parse(fs.readFileSync(destination + '/manifests/config.json', "utf8"));
+  var templateManifest  = JSON.parse(fs.readFileSync(destination + '/manifests/templates.json', "utf8"));
+  var styleManifest   = JSON.parse(fs.readFileSync(destination + '/manifests/styles.json', "utf8"));
+  var applicationSCSS = styleManifest[Object.keys(styleManifest)[0]].match(/application-.*\.css/)[0];
+  return gulp.src(sources.index)
+    .pipe(plugins.plumber({ errorHandler: catchError }))
+    .pipe(plugins.replace('app.js', manifest['app.js']))
+    .pipe(plugins.replace('vendor.js', configManifest['vendor.js']))
+    .pipe(plugins.replace('templates.js', templateManifest['templates.js']))
+    .pipe(plugins.replace('application.css', applicationSCSS))
+    .pipe(plugins.minifyHtml({
+      empty: true,
+      comments: true,
+      quotes: true,
+      spare: true,
+      conditionals: true
+    }))
+    .pipe(gulp.dest(destination));
+}
+
+function views() {
+  var appName = JSON.parse(fs.readFileSync(__dirname + '/../package.json', "utf8")).name;
+  return gulp.src(sources.views, { base: sources.base })
+    .pipe(plugins.plumber({ errorHandler: catchError }))
+    .pipe(plugins.replaceTask({ patterns: [{ json: ENV }] }))
+    .pipe(plugins.minifyHtml({
+      empty: true,
+      comments: true,
+      quotes: true,
+      spare: true,
+      conditionals: true
+    }))
+    .pipe(plugins.html2js({
+      outputModuleName: appName + '.templates',
+      base: 'app/'
+    }))
+    .pipe(plugins.concat('templates.js'))
+    .pipe(plugins.ngAnnotate())
+    .pipe(plugins.uglify())
+    .pipe(plugins.rev())
+    .pipe(gulp.dest(destination))
+    .pipe(plugins.rev.manifest({ path: 'manifests/templates.json' }))
+    .pipe(gulp.dest(destination));
+}
+
+function images() {
+  return gulp.src(sources.images, { base: sources.base })
+    .pipe(plugins.plumber({ errorHandler: catchError }))
+    .pipe(plugins.imagemin({ progressive: true }))
+    .pipe(gulp.dest(destination));
+}
+
+function vendor() {
+  return gulp.src(sources.vendor)
+    .pipe(plugins.include({ extensions: ['js'] }))
+    .pipe(plugins.rev())
+    .pipe(gulp.dest(destination))
+    .pipe(plugins.rev.manifest({ path: 'manifests/config.json' }))
+    .pipe(gulp.dest(destination));
+}
+
+function scripts() {
+  return gulp.src(sources.app)
+    .pipe(plugins.plumber({ errorHandler: catchError }))
+    .pipe(plugins.include({ extensions: ['js'] }))
+    .pipe(plugins.traceur({ modules: 'register' }))
+    .pipe(plugins.ngAnnotate())
+    .pipe(plugins.uglify())
+    .pipe(plugins.replaceTask({ patterns: [{ json: ENV }] }))
+    .pipe(plugins.rev())
+    .pipe(gulp.dest(destination))
+    .pipe(plugins.rev.manifest({ path: 'manifests/scripts.json' }))
+    .pipe(gulp.dest(destination));
+}
+
+function build() {
+  return run(
+    'productionClean',
+    'productionVendor',
+    'productionScripts',
+    'productionStyles',
+    'productionImages',
+    'productionViews',
+    'productionIndex',
+    'productionFonts'
+  );
+}
+
+function buildEnv() {
+  return run('productionSetEnvironment', 'build');
+}
