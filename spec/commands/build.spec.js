7af952187d3f288693bb6d95a74791711f4e7e10
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-02-14 00:18:38 +0200

    test build command

--- spec/commands/build.spec.js
+++ spec/commands/build.spec.js
@@ -1,5 +1,22 @@
+var fs            = require('fs-extra');
+var child_process = require('child_process');
+
 describe('$ kick build', function () {
-  it('should be true', function () {
-    expect(true).toBeTruthy();
+
+  it('SETUP', function () {
+    fs.deleteSync('npm_test');
+    child_process.execSync('kick new npmTest -ns');
+    process.chdir('npm_test');
+  });
+
+  it('should run gulp build task', function () {
+    var output = child_process.execSync('kick build').toString();
+
+    expect(output).toMatch("Building application");
+  });
+
+  it('TEARDOWN', function () {
+    process.chdir('..');
+    fs.deleteSync('npm_test');
   });
 });
