86f1c0581302ba5c7b1af14f7a1fe99fc8ec2097
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-02-13 23:58:47 +0200

    add test for about command

--- spec/commands/about.spec.js
+++ spec/commands/about.spec.js
@@ -1,5 +1,24 @@
+var fs            = require('fs-extra');
+var child_process = require('child_process');
+
 describe('$ kick about', function () {
-  it('should be true', function () {
-    expect(true).toBeTruthy();
+
+  it('SETUP', function () {
+    fs.deleteSync('npm_test');
+    child_process.execSync('kick new npmTest -ns');
+    process.chdir('npm_test');
+  });
+
+  it('should output information about application', function () {
+    var output = child_process.execSync('kick about').toString();
+
+    expect(output).toMatch("NpmTest".blue);
+    expect(output).toMatch("1.0.0".blue);
+    expect(output).toMatch("1.3.12".blue);
+  });
+
+  it('TEARDOWN', function () {
+    process.chdir('..');
+    fs.deleteSync('npm_test');
   });
 });
