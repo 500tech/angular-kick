4bfdbe48e6c732f00365dd958f090aced9924848
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-02-19 18:49:52 +0200

    simplify if-env directive

--- templates/app/app/directives/if_env.js
+++ templates/app/app/directives/if_env.js
@@ -1,17 +1,5 @@
-class ifEnvController {
-  constructor(ENV, $element, $attrs) {
-    if (ENV !== $attrs.ifEnv) {
-      $element.remove();
-    }
-  }
-}
-
-angular.module('%APP_NAME%.directives').directive('ifEnv', function () {
-  return {
-    restrict: 'A',
-    scope: {},
-    controller: ifEnvController,
-    controllerAs: 'ifEnv',
-    bindToController: true
+angular.module('%APP_NAME%.directives').directive('ifEnv', function (ENV) {
+  return function (scope, element, attrs) {
+    if (ENV !== attrs.ifEnv) { element.remove(); }
   }
 });
