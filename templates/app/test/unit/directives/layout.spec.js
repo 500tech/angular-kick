102926bac570ff50ab078fcc1e4c36c120cc71ab
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-02-19 14:37:03 +0200

    add test for layout directive

--- templates/app/test/unit/directives/layout.spec.js
+++ templates/app/test/unit/directives/layout.spec.js
@@ -1,20 +1,22 @@
 describe('layout Directive', function () {
   var element, $scope, createDirective;
 
-  beforeEach(module('%APP_NAME%.config', '%APP_NAME%.directives'));
+  beforeEach(module('%APP_NAME%.config', '%APP_NAME%.services', '%APP_NAME%.directives'));
 
-  beforeEach(inject(function ($rootScope, $compile) {
+  beforeEach(inject(function ($rootScope, $compile, $templateCache, Layout) {
+    $templateCache.put('layouts/application.html', 'APPLICATION');
     createDirective = function (scopeAttrs) {
       $scope = angular.extend($rootScope.$new(), scopeAttrs);
 
       element = angular.element('<layout></layout>');
       element = $compile(element)($scope);
+      Layout.setLayout('application');
       $scope.$apply();
     };
   }));
 
-  it('should be true', function () {
+  it('should show layout', function () {
     createDirective({});
-    expect(true).toBeTruthy();
+    expect(element.html()).toMatch("APPLICATION");
   });
 });
