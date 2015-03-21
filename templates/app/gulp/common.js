b9e186eead1a8847b94b83c3e7e2ac3136379d2f
Ilya Gelman <ilyagelman@ilyagelman.ru>
2015-03-21 04:33:06 +0200

    remove unused variables from gulp common

--- templates/app/gulp/common.js
+++ templates/app/gulp/common.js
@@ -1,29 +1,18 @@
 "use strict";
 
-var fs              = require('fs-extra');
+var fs = require('fs-extra');
 
 module.exports = {
   sources: {
-    error:         __dirname + '/error.html',
-    base:         'app',
-
-    app:          'app/app.js',
-    modules:      'app/modules.js',
-    configs:      'app/config/**/*.js',
-    directives:   'app/directives/**/*.js',
-    filters:      'app/filters/**/*.js',
-    models:       'app/models/**/*.js',
-    services:     'app/services/**/*.js',
-    controllers:  ['app/states/**/*.js', 'app/layouts/**/*.js'],
-
-    scripts:      'app/**/*.js',
-    styles:       'app/assets/stylesheets/**/*.scss',
-    mainStyle:    'app/assets/stylesheets/application.scss',
-
-    images:       'app/assets/images/**',
-    fonts:        'app/assets/fonts/**',
-    views:        ['app/**/*.html', '!app/app.html'],
-    index:        'app/app.html'
+    error:      __dirname + '/error.html',
+    base:      'app',
+    scripts:   'app/**/*.js',
+    styles:    'app/assets/stylesheets/**/*.scss',
+    mainStyle: 'app/assets/stylesheets/application.scss',
+    images:    'app/assets/images/**',
+    fonts:     'app/assets/fonts/**',
+    views:     ['app/**/*.html', '!app/app.html'],
+    index:     'app/app.html'
   },
 
   destinations: {
