24ca1115e583e83b62cb9ba1f1647ebec13d5ab0
nirkaufman <nirkaufman@gmail.com>
2015-04-06 00:11:06 -0400

    refactoring gulp test commands to reuse the same karma.conf file

--- templates/app/gulp/development/tdd.js
+++ templates/app/gulp/development/tdd.js
@@ -1,6 +1,7 @@
 var karma       = require('karma').server;
 
 module.exports = function (done) {
+  global.karmaBaseDirectory = '.tmp';
   return karma.start({
     configFile: __dirname + '/../../karma.conf.js',
     singleRun: false
