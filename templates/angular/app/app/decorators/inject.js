fd6d1961e92774f57cdee5507c62e1241d12ae67
Ilya Gelman <ilyagelman@ilyagelman.com>
2015-10-21 12:01:59 +0300

    Replace JSDoc with code comment

--- templates/angular/app/app/decorators/inject.js
+++ templates/angular/app/app/decorators/inject.js
@@ -1,10 +1,4 @@
-/**
- * annotate the target class with injectables.
- *
- * @param injectables - angular providers to inject
- * @type string
- * @returns {Function} decorator function
- */
+// Annotate the target class with injectables
 export function Inject(...injectables) {
   return function injector(target) {
     target.$inject = [...injectables];
