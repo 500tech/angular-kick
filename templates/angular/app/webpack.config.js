966afe29ce13e2f60c7bfad0d84c555babec0901
Maayan Glikser <maayan@glikm.com>
2015-10-21 10:34:51 +0300

    Update webpack config to ES6

--- templates/angular/app/webpack.config.js
+++ templates/angular/app/webpack.config.js
@@ -1,58 +1,99 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var config = {
+/* global __dirname, process */
+
+const webpack           = require('webpack');
+const path              = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const environmentsFile  = path.join(__dirname, '/environments.json');
+
+const config = {
 
   // set the context (optional)
-  context: __dirname + '/app',
+  context: path.join(__dirname, '/app'),
   entry: {
     app: 'app.js',
-    vendor: ['angular', 'angular-ui-router']
+    vendor: ['angular', 'angular-ui-router'],
   },
 
   output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
   },
 
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName: */'vendor', /* filename: */'vendor.[hash].js'),
+    new webpack.optimize.CommonsChunkPlugin(
+      /* chunkName: */ 'vendor',
+      /* filename: */ 'vendor.[hash].js'
+    ),
     new HtmlWebpackPlugin({
       inject: 'body',
-      template: 'app/index.html'
-    })
+      template: 'app/index.html',
+    }),
   ],
 
   // enable loading modules relatively (without the ../../ prefix)
   resolve: {
-    root: [__dirname + '/app']
+    root: [path.join(__dirname, '/app')],
   },
 
   module: {
     preLoaders: [
       {
         test: /\.*/,
-        loaders: ['env-replace?prefix=@@&file=' + __dirname + '/environments.json']
+        loaders: [
+          `env-replace?prefix=@@&file=${environmentsFile}`
+        ]
       }
     ],
     loaders: [
+
       // Transpile ES6 and annotate AngularJS dependencies
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate', 'babel'] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'ng-annotate',
+          'babel'
+        ]
+      },
 
       // SCSS
-      { test: /\.s?css$/, loader: 'style!css!autoprefixer!sass?' + 'includePaths[]=' + __dirname + '/app'},
+      {
+        test: /\.s?css$/,
+        loaders: [
+          'style',
+          'css',
+          'autoprefixer',
+          `sass?includePaths[]=${path.join(__dirname, '/app')}`
+        ]
+      },
 
-       //JSON
-      { test: /\.json$/, loader: 'json' },
+      // JSON
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
 
       // Fonts and images
-      { test: /\.(ttf|eot|svg|otf|png)$/, loader: 'file' },
-      { test: /\.(png)$/, loader: 'url?mimetype=image/png' },
-      { test: /\.woff(2)?$/, loader: 'url?limit=10000&minetype=application/font-woff'},
+      {
+        test: /\.(ttf|eot|svg|otf|png)$/,
+        loader: 'file'
+      },
+      {
+        test: /\.(png)$/,
+        loader: 'url?mimetype=image/png'
+      },
+      {
+        test: /\.woff(2)?$/,
+        loader: 'url?limit=10000&minetype=application/font-woff'
+      },
 
       // Create AngularJS templates from HTMLs
       {
         test: /\.html$/,
-        loaders: ['ngtemplate?relativeTo=' + __dirname + '/app', 'html']
+        loaders: [
+          `ngtemplate?relativeTo=${path.join(__dirname, '/app')}`,
+          'html'
+        ]
       }
     ]
   },
