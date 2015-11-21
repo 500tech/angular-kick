273b4057cf85d775d0bd640ec03b6a52886131d3
Ilya Gelman <ilyagelman@ilyagelman.com>
2015-11-21 18:32:27 +0200

    remove unneeded ngInject from factory classes

--- templates/angular/model-factory.js
+++ templates/angular/model-factory.js
@@ -1,6 +1,5 @@
 class %MODEL_NAME% {
 
-  // @ngInject
   constructor($log) {
     this.$log = $log;
     this.message = '%MODEL_NAME%';
--- templates/angular/service-factory.js
+++ templates/angular/service-factory.js
@@ -1,6 +1,5 @@
 class %SERVICE_NAME% {
 
-  // @ngInject
   constructor($log) {
     this.$log = $log;
     this.message = '%SERVICE_NAME%';
